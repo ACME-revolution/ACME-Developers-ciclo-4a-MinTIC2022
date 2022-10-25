238b7984a3c6ae79a9a84f8d26f673b71552c994
Nicol Guevara <111058276+Neofitar@users.noreply.github.com>
2022-10-24 21:39:15 -0500

    Home
    Diseño de home

--- front/src/components/Cabecera.js
+++ front/src/components/Cabecera.js
@@ -3,27 +3,128 @@ import React, { Fragment } from 'react'
 const Cabecera = () => {
   return (
     <Fragment>
-        <nav className= 'barra navegacion'>
-            <div className='col-12 col-md-3'>
-                <div className='navbar-brand'>
-                    <img src=".\images\logoAcme.jpg" alt= "Logo ACME Corporation"></img>
+        <header>
+            <nav className="navbar row">
+                    <div className="col-10 col-md-1">
+                        <div className="navbar-brand">
+                        <img src=".\images\logoAcme.jpg" width="200" height="200" alt= "Logo ACME Corporation"></img>
+                    </div>
                 </div>
-            </div>
 
-            <div className='col-12 col-md-6 mt-2 mt-md-0'>
-                <div className='cajon-busqueda'>
-                    <input
+                <div className="col-12 col-md-6 mt-1 mt-md-0">
+                    <div class= "input-group">
+                        <input
                             type="text"
                             id="search_field"
                             class="form-control"
-                            placeholder='¿Qué producto está buscando?'></input>
+                            placeholder="¿Qué producto está buscando?"></input>
+                        <div class="input-group-append">
+                            <button id="search-btn" class="btn">
+                                <i class="fa fa-search fa-1x text-white" aria-hidden="true"></i>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-12 col-md-3 mt-4 mt-md-0 text-center">
+                    <span><button className='btn' id="login_btn">Iniciar sesión</button></span>
+                    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+                    <i class="fa fa-shopping-cart fa-1x text-white" aria-hidden="false"></i>
+                    &nbsp;
+                    <span className='ml-1' id="cart_count">2</span>
+                </div>
+            </nav>
+        </header>
+
+        <section id="productos" className='container mt-5'>
+                <div className='row'>
+                    {/*Producto 1*/}
+                    <div className='col-sm-12 col-md-6 col-lg-3 my-3'>
+                        <div className='card p-3 rounded'>
+                            <img className='card-img-top mx-auto' src='.\images\producto1_junke.jpg' alt="Producto1"></img>
+                            <div className='card-body d-flex flex-column'>
+                                <h5 id="titulo_producto"><a href='http://localhost:3000'>Yunque de hierro</a></h5>
+                                <div className='rating mt-auto'>
+                                    <div className='rating-outer'>
+                                        <div className='rating-inner'></div>
+                                    </div>
+                                    <span id="No_de_opiniones"> 5 reviews</span>
+                                </div>
+                                <p className='card-text'>$72.000</p><a href='http://localhost:3000' id="view_btn" className='btn btn-block'>
+                                    Ver detalle
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/*Producto 2*/}
+                    <div className='col-sm-12 col-md-6 col-lg-3 my-3'>
+                        <div className='card p-3 rounded'>
+                            <img className='card-img-top mx-auto' src='.\images\producto2_tornado.jpg' alt="Producto2"></img>
+                            <div className='card-body d-flex flex-column'>
+                                <h5 id="titulo_producto"><a href='http://localhost:3000'>Tornado KIT</a></h5>
+                                <div className='rating mt-auto'>
+                                    <div className='rating-outer'>
+                                        <div className='rating-inner'></div>
+                                    </div>
+                                    <span id="No_de_opiniones"> 2 reviews</span>
+                                </div>
+                                <p className='card-text'>$72.000</p><a href='http://localhost:3000' id="view_btn" className='btn btn-block'>
+                                    Ver detalle
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/*Producto 3*/}
+                    <div className='col-sm-12 col-md-6 col-lg-3 my-3'>
+                        <div className='card p-3 rounded'>
+                            <img className='card-img-top mx-auto' src='./images/producto3_iman.jpg' alt="Producto3"></img>
+                            <div className='card-body d-flex flex-column'>
+                                <h5 id="titulo_producto"><a href='http://localhost:3000'>Imán</a></h5>
+                                <div className='rating mt-auto'>
+                                    <div className='rating-outer'>
+                                        <div className='rating-inner'></div>
+                                    </div>
+                                    <span id="No_de_opiniones"> 12 reviews</span>
+                                </div>
+                                <p className='card-text'>$20.000</p><a href='http://localhost:3000' id="view_btn" className='btn btn-block'>
+                                    Ver detalle
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/*Producto 4*/}
+                    <div className='col-sm-12 col-md-6 col-lg-3 my-3'>
+                        <div className='card p-3 rounded'>
+                            <img className='card-img-top mx-auto' src='./images/producto4_pills.jpg' alt="Producto4"></img>
+                            <div className='card-body d-flex flex-column'>
+                                <h5 id="titulo_producto"><a href='http://localhost:3000'>Somníferos en pastillas</a></h5>
+                                <div className='rating mt-auto'>
+                                    <div className='rating-outer'>
+                                        <div className='rating-inner'></div>
+                                    </div>
+                                    <span id="No_de_opiniones"> 7 reviews</span>
+                                </div>
+                                <p className='card-text'>$15.000</p><a href='http://localhost:3000' id="view_btn" className='btn btn-block'>
+                                    Ver detalle
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
 
                 </div>
-            </div>
+            </section>
 
-
-        </nav>                
+        <footer>
+            <p className="text-center mt-1">
+                ACME Store | 2022 - Mision TIC | © Todos los derechos reservados.
+            </p>
+        </footer>
     </Fragment>
+
   )
 }
 
