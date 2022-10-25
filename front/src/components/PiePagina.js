e022e5deb5d1a91ec0ae8cba4eab90fcc1faa139
Nicol Guevara <111058276+Neofitar@users.noreply.github.com>
2022-10-24 22:14:17 -0500

    Enrutación de home

---
+++ front/src/components/PiePagina.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react'
+
+const PiePagina = () => {
+  return (
+    <Fragment>
+        <footer>
+            <p className="text-center mt-1">
+                ACME Store | 2022 - Mision TIC | © Todos los derechos reservados.
+            </p>
+        </footer>
+    </Fragment>
+
+  )
+}
+
+export default PiePagina
